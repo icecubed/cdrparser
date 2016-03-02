8da99112d424c8215773249e53c69f9d633cb98b
Joel Alenchery <joel@alenchery.org>
2016-03-02 11:48:24 +0530

    add more error checking

--- exp.js
+++ exp.js
@@ -15,6 +15,10 @@ var output      = fs.createWriteStream('./output/uuid.csv');
 var stringifier = stringify({ header: true });
 //var v1APIBase   = 'https://beta.deltamktgresearch.com/api/cdr?uuid=';
 var v1APIBase   = 'https://voicereach365.com/api/cdr?uuid=';
+var fileLocation = {
+  prefix: '/usr/local/share/freeswitch/logs/xml_cdr/',
+  postfix: '.cdr.xml'
+};
 
 function writeLog(logs, isError){
   logs.push('');
@@ -32,15 +36,16 @@ var transformer = transform(function(record, callback){
   var log = [];
 
   log.push('Processing : ' + record.uuid);
+  var location = fileLocation.prefix + record.uuid + fileLocation.postfix;
 
   record.status      = '';
   record.exception   = '';
   record.statusCode  = '';
   record.responseTxt = '';
 
-  log.push('reading file  : ' + record.location);
+  log.push('reading file  : ' + location);
 
-  fs.readFile(record.location, 'utf-8', function(err, xml) {
+  fs.readFile(location, 'utf-8', function(err, xml) {
     if (err) {
       record.status = 'file-read-error';
       record.exception = util.inspect(err, {depth: 4});
@@ -51,36 +56,93 @@ var transformer = transform(function(record, callback){
     }
 
     var xmlParser = new xml2js.Parser();
-    parser.parseString(data, function (err, result) {
-        console.dir(result);
-    });
-    log.push('reading complete, sending file to V1 api @ ' + v1APIBase + record.uuid);
-    writeLog(log);
-    request
-      .post(v1APIBase + record.uuid)
-      .type('form')
-      .send({
-        cdr: xml
-      })
-      .end(function(err, response) {
-        if (err) {
-          record.status = 'post-request-error';
-          record.exception = util.inspect(err, {depth: 4});
-          callback(null, record);
-          log.push(util.inspect(record, {depth: 4, colors: true}));
-          writeLog(log, true);
-          return;
-        }
-
-        log.push('server response received: ' + record.uuid);
-        record.status      = 'success';
-        record.statusCode  = response.statusCode;
-        record.responseTxt = response.text;
+    xmlParser.parseString(xml, function (err, result) {
+      if (err) {
+        record.status = 'xml-file-parse-error';
+        record.exception = util.inspect(err, {depth: 4});
         callback(null, record);
         log.push(util.inspect(record, {depth: 4, colors: true}));
-        writeLog(log, false);
+        writeLog(log, true);
         return;
-      });
+      }
+
+      if(result.cdr){
+        if(result.cdr.variables && result.cdr.variables.length){
+          if(!result.cdr.variables[0].ls_con_ref && !result.cdr.variables[0].sip_h_pro_username){
+            log.push('finding producer UUID');
+            var producerUUID = result.cdr.variables[0].originator[0];
+            log.push('found producerUUID: ' + producerUUID);
+            location = fileLocation.prefix + producerUUID + fileLocation.postfix;
+            fs.readFile(location, 'utf-8', function(err, producerXml) {
+              if (err) {
+                record.status = 'file-read-error';
+                record.exception = util.inspect(err, {depth: 4});
+                callback(null, record);
+                log.push(util.inspect(record, {depth: 4, colors: true}));
+                writeLog(log, true);
+                return;
+              }
+
+              log.push('reading complete, sending file to V1 api @ ' + v1APIBase + producerUUID);
+              writeLog(log);
+              request
+                .post(v1APIBase + producerUUID)
+                .type('form')
+                .send({
+                  cdr: producerXml
+                })
+                .end(function(err, response) {
+                  if (err) {
+                    record.status = 'post-request-error';
+                    record.exception = util.inspect(err, {depth: 4});
+                    callback(null, record);
+                    log.push(util.inspect(record, {depth: 4, colors: true}));
+                    writeLog(log, true);
+                    return;
+                  }
+
+                  log.push('server response received: ' + record.uuid);
+                  record.status      = 'success';
+                  record.statusCode  = response.statusCode;
+                  record.responseTxt = response.text;
+                  callback(null, record);
+                  log.push(util.inspect(record, {depth: 4, colors: true}));
+                  writeLog(log, false);
+                  return;
+                });
+            });
+          } else {
+            log.push('reading complete, sending file to V1 api @ ' + v1APIBase + record.uuid);
+            writeLog(log);
+            request
+              .post(v1APIBase + record.uuid)
+              .type('form')
+              .send({
+                cdr: xml
+              })
+              .end(function(err, response) {
+                if (err) {
+                  record.status = 'post-request-error';
+                  record.exception = util.inspect(err, {depth: 4});
+                  callback(null, record);
+                  log.push(util.inspect(record, {depth: 4, colors: true}));
+                  writeLog(log, true);
+                  return;
+                }
+
+                log.push('server response received: ' + record.uuid);
+                record.status      = 'success';
+                record.statusCode  = response.statusCode;
+                record.responseTxt = response.text;
+                callback(null, record);
+                log.push(util.inspect(record, {depth: 4, colors: true}));
+                writeLog(log, false);
+                return;
+              });
+          }
+        }
+      }
+    });
   });
 }/*, {parallel: 10}*/);
 
